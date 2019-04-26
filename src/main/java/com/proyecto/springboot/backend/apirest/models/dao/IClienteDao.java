6c57f39cb3922c72eb3ecf9ae22891357122a6da
christianesk <christian.91.gm@hotmail.com>
2019-04-26 15:55:54 -0500

    Rest api Clientes springboot 5

---
+++ src/main/java/com/proyecto/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.proyecto.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyecto.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+
+}
---
+++ src/main/java/com/proyecto/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,15 @@
+package com.proyecto.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.proyecto.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+}
